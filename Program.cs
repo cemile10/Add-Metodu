eedfebdee8e2874b715b9b1ed89b4f3b7a5f3d4d
cemile10 <c3van0ri@gmail.com>
2024-02-26 11:45:46 +0400

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class ArrayMethods
+{
+  
+    public static void Add()
+    {
+        int[] arr = new int[10]; 
+        int count = 0; 
+
+        Console.WriteLine("Ededler daxil edin,daxil etmeyi bitirmek ucun hecne yazmadan entere basin");
+
+        while (true) 
+        {
+            if (count == arr.Length) 
+            {
+                int[] yeni_array = new int[arr.Length * 2]; 
+                Array.Copy(arr, yeni_array, arr.Length); 
+                arr = yeni_array; 
+            }
+
+            string input = Console.ReadLine(); 
+            if (string.IsNullOrWhiteSpace(input)) 
+                break;
+
+            if (int.TryParse(input, out int number)) 
+            {
+                arr[count] = number; 
+                count++; 
+            }
+            else
+            {
+                Console.WriteLine("Eded yazmalisiniz"); 
+            }
+        }
+
+        Console.WriteLine("Arraydeki ededler:"); 
+        for (int i = 0; i < count; i++) 
+        {
+            Console.Write(arr[i] + " ");
+        }
+    }
+}
+namespace MyProgram
+{
+    class MetodYoxlamalari
+    {
+        static void Main(string[] args)
+        {
+            ArrayMethods.Add();
+        }
+    }
+}  
